e05b583dd6e9dbcd3dbb0b0560dfc6944be98134
Avanish-10 <gaonkaravanish10@gmail.com>
2025-11-15 11:33:35 +0530

    version 1 restored

--- amend.js
+++ amend.js
@@ -3,7 +3,8 @@ console.log('--amend');
 // git init
 // git add .
 // git commit -m " NAME"
-// git checkout 
+// git checkout [hash] // move to that version
+// git checkout [hash] . // move to that version to current branch
 // git log --all --graph
 // git log --all 
-
+// git config --global alias.s "status"
